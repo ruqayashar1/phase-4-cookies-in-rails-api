eca63ed30577051ad4c3181a0f43b4c276c53a55
rukia <ruqayashar1@gmail.com>
2023-07-08 12:13:50 +0300

    seesion api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
-  
+  byebug
   def index
-    session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    session[:session_hello] ||= "Welcome"
+    cookies[:cookies_hello] ||= "Welcome"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
